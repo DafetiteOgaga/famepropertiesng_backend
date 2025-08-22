053708c818543c942dcde63cebdb70070ac1e4d3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-22 15:31:47 +0100

    models.py: minor fix

--- famouspropertiesng/featuresadvert/models.py
+++ famouspropertiesng/featuresadvert/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here..
 class FeatureAdvert(models.Model):
     anchor = models.CharField(max_length=200)
     paragraph = models.TextField()
