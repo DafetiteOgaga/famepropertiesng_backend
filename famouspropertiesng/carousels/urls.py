e0e934a1dd6d0c499fccacb654057a9010f57e8d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-08-22 02:19:37 +0100

    urls.py: fixed type in path endpoint

--- famouspropertiesng/carousels/urls.py
+++ famouspropertiesng/carousels/urls.py
@@ -6,5 +6,5 @@ app_name = "carousels"
 urlpatterns = [
 	# Create your urlpatterns here.
 	path("carousels/", views.carousels, name="carousel_list_create"),
-	path("delete-carousel/", views.deleteCarousel, name="carousel_list_create"),
+	path("delete-carousels/", views.deleteCarousel, name="carousel_list_create"),
 ]
