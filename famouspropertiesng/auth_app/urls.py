f233c08050999c63761b4952747961310405ae86
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-02 15:11:07 +0100

    urls.py: removed unused auth endpoint

--- famouspropertiesng/auth_app/urls.py
+++ famouspropertiesng/auth_app/urls.py
@@ -10,7 +10,7 @@ app_name = "auth_app"
 urlpatterns = [
 	# Create your urlpatterns here.
 	path("api/auth/google/", views.google_login, name="google_login"),
-	path("api/authenticate/", views.generate_signature, name="generate_signature"),
+	# path("api/authenticate/", views.generate_signature, name="generate_signature"),
 	path("imagekit-auth/", views.imagekit_auth, name="imagekit_auth"),
 	# path("products/", views.products, name="product_list_create"),
 	# path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # login
