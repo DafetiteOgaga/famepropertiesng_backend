7860255243f9c84e6f9360e9800ea68ed17b6add
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-05 14:18:06 +0100

    models.py:

---
+++ famouspropertiesng/productrating/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class ProductRating(models.Model):
+    # A user can rate a product only once
+	product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='prod_ratings')
+	user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='product_ratings')
+	rating = models.PositiveIntegerField(blank=True, null=True)  # Assuming rating is an integer value
+	liked = models.BooleanField(default=False)
+	review = models.TextField(blank=True, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		# user cannot rate the same product more than once
+		constraints = [
+			models.UniqueConstraint(fields=['product', 'user'], name='unique_product_user')
+		]
+
+	def __str__(self):
+		return f'{self.product.name} rated by {self.user.first_name}'
