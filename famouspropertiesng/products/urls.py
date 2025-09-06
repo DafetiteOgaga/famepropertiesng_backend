dc915961eb20bbcea3b795fc48461d58cdb419ed
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-07 00:04:25 +0100

    urls.py: fixed path converter ordering

--- famouspropertiesng/products/urls.py
+++ famouspropertiesng/products/urls.py
@@ -6,8 +6,8 @@ app_name = "products"
 urlpatterns = [
 	# Create your urlpatterns here.
 	path("products/", views.products, name="product_list_create"),
-	path("products/<str:all>/", views.products, name="product_list_all"),
 	path("products/<int:pk>/", views.products, name="product_list_create"),
+	path("products/<str:all>/", views.products, name="product_list_all"),
 	path("delete-products/", views.deleteProduct, name="delete_product"),
 	path("sold-products/<int:pk>/", views.designateAsSold, name="sold_product"),
 	path("like-product/<int:pk>/", views.likeProduct, name="like_product"),
