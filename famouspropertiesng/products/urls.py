6412d884c1a77a958f3496c3ff0fd923733138d3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-06 22:34:45 +0100

    urls.py: added a secindary endpoint to fetch unpaginated products

--- famouspropertiesng/products/urls.py
+++ famouspropertiesng/products/urls.py
@@ -6,6 +6,7 @@ app_name = "products"
 urlpatterns = [
 	# Create your urlpatterns here.
 	path("products/", views.products, name="product_list_create"),
+	path("products/<str:all>/", views.products, name="product_list_all"),
 	path("products/<int:pk>/", views.products, name="product_list_create"),
 	path("delete-products/", views.deleteProduct, name="delete_product"),
 	path("sold-products/<int:pk>/", views.designateAsSold, name="sold_product"),
