56e9781d77403b0510498a787073b540bc0c3807
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-13 00:10:39 +0100

    serializers.py:

---
+++ famouspropertiesng/store/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import *
+from users.models import User
+
+# Create your serializers here.
+class StoreSerializer(serializers.ModelSerializer):
+	class InlineUserSerializer(serializers.ModelSerializer):
+		class Meta:
+			model = User
+			fields = [
+				"id", "email", "mobile_no",
+				"first_name", "last_name"
+			]  # keep it small
+
+	user = InlineUserSerializer(read_only=True)
+
+	class Meta:
+		model = Store
+		fields = [
+			'id', 'user', 'store_name',
+			'nearest_bus_stop', 'description',
+			'store_status', 'verified', 'rating'
+		]
+
