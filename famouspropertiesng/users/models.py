3846b75f0995a7940893827b0d69df6963108cf1
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-20 20:43:57 +0100

    models.py: indexed email in db level

--- famouspropertiesng/users/models.py
+++ famouspropertiesng/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
 	# middle_name = models.CharField(max_length=100, null=True, blank=True)
-	email = models.EmailField(max_length=200, unique=True)
+	email = models.EmailField(max_length=200, unique=True, db_index=True)
 	mobile_no = models.CharField(max_length=20)
 	address = models.CharField(max_length=200)
 
