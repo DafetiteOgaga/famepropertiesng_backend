8889afa4096c135e27c0723cbe8901266fcd3510
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-09-20 13:53:20 +0100

    models.py: fix1 for checkout model: added checkoutID, created and updated at fields

--- famouspropertiesng/checkouts/models.py
+++ famouspropertiesng/checkouts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
+from django.db import models, IntegrityError, transaction
 
 # Create your models here.
+class Checkout(models.Model):
+	checkoutID = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return f"Checkout {self.id}"
