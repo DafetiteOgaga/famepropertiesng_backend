7d42e44cae440917ebd5de8180b4caa1106dcdab
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-01 11:00:55 +0100

    update_number_of_items.py: changed number_of_items to number_of_items_available

--- famouspropertiesng/hooks/update_number_of_items.py
+++ famouspropertiesng/hooks/update_number_of_items.py
@@ -1,14 +1,14 @@
 from products.models import Product
 import random
 
-def update_number_of_items():
+def update_number_of_items_available():
     """
-    Loops through the products and updates the number_of_items field randomly
+    Loops through the products and updates the number_of_items_available field randomly
     """
     products = Product.objects.all()
     for product in products:
-        product.numberOfItems = random.randint(20, 100)
+        product.numberOfItemsAvailable = random.randint(20, 100)
         product.save()
-        print(f"Updated {product.name}: numberOfItems = {product.numberOfItems}")
+        print(f"Updated {product.name}: numberOfItemsAvailable = {product.numberOfItemsAvailable}")
 
-update_number_of_items() # Call run to execute when the script is run directly
+update_number_of_items_available() # Call run to execute when the script is run directly
