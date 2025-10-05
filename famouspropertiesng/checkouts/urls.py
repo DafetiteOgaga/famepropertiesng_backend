c774d9ef2aef7a05cd0318ab470f7824189f5dde
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-05 19:25:18 +0100

    urls.py: added endpoints needed for checkouts/payments to be seamless

--- famouspropertiesng/checkouts/urls.py
+++ famouspropertiesng/checkouts/urls.py
@@ -6,4 +6,12 @@ app_name = "checkouts"
 urlpatterns = [
 	# Create your urlpatterns here.
 	path('checkouts/', views.checkouts, name='checkouts'),
+	# path('verify-payment/', views.verify_payment, name='verify-payment'),
+	# path('generate-reference/', views.generate_reference, name="generate_reference"),
+	path("receipt/<str:reference>/", views.checkout_receipt_view, name="receipt"),
+	path("installment-payment/<str:reference>/", views.installment_payment, name="receipt"),
+	path("paystack-webhook/", views.paystack_webhook, name="paystack_webhook"),
+	path('checkout-status/<str:reference>/', views.checkout_status, name='checkout-status'),
+	path("get-unfulfilled-checkout-ids/<int:pk>/", views.get_unfulfilled_checkout_ids, name="get-unfulfilled-checkout-ids"),
+	path("has-unfulfilled-installments/<int:pk>/", views.has_unfulfilled_installments, name="has-unfulfilled-installments"),
 ]
