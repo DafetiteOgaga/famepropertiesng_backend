3062dc8681626842ef0fa18703a5f00b731881bd
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-12 21:41:46 +0100

    prettyprint.py: fixed exception errors

--- famouspropertiesng/hooks/prettyprint.py
+++ famouspropertiesng/hooks/prettyprint.py
@@ -5,8 +5,8 @@ def pretty_print_json(data):
     Takes a JSON string or Python dict/list and prints it in a pretty format.
     """
     # If it's a string, parse it into a Python object
-    if isinstance(data, str):
+    if not isinstance(data, object):
         data = json.loads(data)
 
     # Pretty print with indentation
-    print(json.dumps(data, indent=4, sort_keys=True))
+    print(json.dumps(data, indent=4, sort_keys=True, default=str))
