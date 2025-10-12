6a954e549f2d45f8629afdc158b47e2ed4c694b6
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-12 10:48:31 +0100

    urls.py: fixed up verify-payment and generate-reference endpoints

--- famouspropertiesng/checkouts/urls.py
+++ famouspropertiesng/checkouts/urls.py
@@ -6,8 +6,8 @@ app_name = "checkouts"
 urlpatterns = [
 	# Create your urlpatterns here.
 	path('checkouts/', views.checkouts, name='checkouts'),
-	# path('verify-payment/', views.verify_payment, name='verify-payment'),
-	# path('generate-reference/', views.generate_reference, name="generate_reference"),
+	path('verify-payment/<str:reference>/', views.verify_paystack_payment, name='verify-payment'),
+	path('generate-reference/', views.generate_reference, name="generate_reference"),
 	path("receipt/<str:reference>/", views.checkout_receipt_view, name="receipt"),
 	path("installment-payment/<str:reference>/", views.installment_payment, name="receipt"),
 	path("paystack-webhook/", views.paystack_webhook, name="paystack_webhook"),
