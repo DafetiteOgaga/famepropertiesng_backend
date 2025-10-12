a03801a75478a23a7812f1ca3e828361f37e9825
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-12 10:47:44 +0100

    serializers.py: fixed seializers and added CheckoutWithProductSerializer

--- famouspropertiesng/checkouts/serializers.py
+++ famouspropertiesng/checkouts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Checkout, CheckoutProduct, InstallmentPayment
 from users.serializers import UserSerializerWRatings
+from products.serializers import ProductWOCatSerializer
 
 # Create your serializers here.
 class CheckoutProductSerializer(serializers.ModelSerializer):
@@ -23,48 +24,54 @@ class CheckoutSerializer(serializers.ModelSerializer):
 
 
 class ReceiptCheckoutProductSerializer(serializers.ModelSerializer):
-    product_id = serializers.IntegerField(source='product.id', read_only=True)
-    product_name = serializers.CharField(source='product.name', read_only=True)
-    product_thumbnail = serializers.URLField(source='product.image_url_0', read_only=True)
-    class Meta:
-        model = CheckoutProduct
-        fields = ['product_id', 'quantity', 'price', 'product_name', 'product_thumbnail']
+	product_id = serializers.IntegerField(source='product.id', read_only=True)
+	product_name = serializers.CharField(source='product.name', read_only=True)
+	product_thumbnail = serializers.URLField(source='product.image_url_0', read_only=True)
+	class Meta:
+		model = CheckoutProduct
+		fields = ['product_id', 'quantity', 'price', 'product_name', 'product_thumbnail']
 
 class ReceiptInstallmentPaymentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = InstallmentPayment
-        fields = ['reference', 'amount_paid', 'transaction_id', 'payment_date', 'status']
+	class Meta:
+		model = InstallmentPayment
+		fields = ['reference', 'amount_paid', 'transaction_id', 'payment_date', 'status']
 
 class ReceiptCheckoutReceiptSerializer(serializers.ModelSerializer):
-    products = ReceiptCheckoutProductSerializer(source='rn_checkout_products', many=True)
-    installments = ReceiptInstallmentPaymentSerializer(source='rn_installments', many=True)
-    # receipt_url = serializers.SerializerMethodField()
+	products = ReceiptCheckoutProductSerializer(source='rn_checkout_products', many=True)
+	installments = ReceiptInstallmentPaymentSerializer(source='rn_installments', many=True)
+	# receipt_url = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Checkout
-        fields = [
-            'checkoutID',
-            'first_name',
-            'last_name',
-            'email',
-            'phone_code',
-            'mobile_no',
-            'address',
-            'city',
-            'state',
-            'country',
-            'subtotal_amount',
-            'shipping_fee',
-            'total_amount',
-            'remaining_balance',
-            'payment_status',
-            'payment_method',
-            # 'receipt_url',
-            'transaction_id',
-            'products',
-            'installments',
-            'created_at',
-        ]
+	class Meta:
+		model = Checkout
+		fields = [
+			'checkoutID',
+			'first_name',
+			'last_name',
+			'email',
+			'phone_code',
+			'mobile_no',
+			'address',
+			'city',
+			'state',
+			'country',
+			'subtotal_amount',
+			'shipping_fee',
+			'total_amount',
+			'remaining_balance',
+			'payment_status',
+			'payment_method',
+			# 'receipt_url',
+			'transaction_id',
+			'products',
+			'installments',
+			'created_at',
+		]
 
-    # def get_receipt_url(self, obj):
-    #     return obj.receipt_url  # uses your unified property
+	# def get_receipt_url(self, obj):
+	#     return obj.receipt_url  # uses your unified property
+
+class CheckoutWithProductSerializer(serializers.ModelSerializer):
+	product = ProductWOCatSerializer(read_only=True)
+	class Meta:
+		model = CheckoutProduct
+		fields = '__all__'
