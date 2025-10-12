ea70b8854999133af898904302c48d9134a9ce84
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-12 15:42:19 +0100

    urls.py:

---
+++ famouspropertiesng/search_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "search_app"
+
+urlpatterns = [
+	# Create your urlpatterns here.
+	path("check-email/<str:email>/", views.check_email, name="check_email"),
+	path('store/check-store-name/<str:name>/', views.check_store_name, name='check_store_name'),  # For checking store name availability
+	path('store/check-store-email/<str:email>/', views.check_store_email, name='check_store_email'),  # For checking store email availability
+]
