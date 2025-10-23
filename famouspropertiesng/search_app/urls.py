cd46d113a6a902837ad59acc774a8b707e514e3b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-10-23 19:29:09 +0100

    urls.py: added search endpoint

--- famouspropertiesng/search_app/urls.py
+++ famouspropertiesng/search_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path("check-email/<str:email>/", views.check_email, name="check_email"),
 	path('store/check-store-name/<str:name>/', views.check_store_name, name='check_store_name'),  # For checking store name availability
 	path('store/check-store-email/<str:email>/', views.check_store_email, name='check_store_email'),  # For checking store email availability
+	path('search/<int:pk>/<str:s_text>/', views.search_data, name='search_data'),  # For searching products
 ]
